f87af8409dcad587c0936fc48d6c7fb8adc18f68
Bharadwaj Duggaraju <69485164+bharadwajduggaraju@users.noreply.github.com>
2021-08-01 14:12:40 -0700

    Update jsx.ts

--- src/references/jsx.ts
+++ src/references/jsx.ts
@@ -1,6 +1,10 @@
 declare namespace JSX {
   interface IntrinsicElements {
     foo: any;
+    makeOwnIntrinsic: {isCool : boolean, notNeeded ?: boolean}
+  }
+
+  interface ElementClass {
   }
 }
 
@@ -35,3 +39,55 @@ function CustomH1(props: {color: string, size: string, children: any}): JSX.Elem
 <ComponentFoo name="" X={1} Y={2}/>
 <CustomH1 color="red" size="10rem">hey!</CustomH1>
 </>
+
+const propsInObjectForm = {isCool: true}
+
+class ClassReactComponent extends React.Component {
+    render() {
+        return (
+            <div>hey!</div>
+        )
+    }
+}
+
+class ClassComponentWithProps extends React.Component<{message: string}> {
+  render() {
+    return (
+      <div> Message Is {this.props.message}</div>
+    )
+  }
+}
+
+function ChildrenChecking(props: {children: (a: string) => JSX.Element}) {
+  return (
+    <div>
+      {props.children("Hey!")}
+    </div>
+  )
+}
+
+<main>
+<ClassReactComponent />
+<makeOwnIntrinsic isCool notNeeded={false}/>
+<makeOwnIntrinsic {...propsInObjectForm}/>
+<ClassComponentWithProps message="Hello World!!"/>
+<ChildrenChecking>
+  {(a) => {
+    return (
+      <div>Hey the message is ${a}</div>
+    )
+  }}
+</ChildrenChecking>
+</main>
+
+// React Integation
+/// <reference path="react.d.ts" />
+interface Props {
+  foo: string;
+}
+class MyComponent extends React.Component<Props, {}> {
+  render() {
+    return <span>{this.props.foo}</span>;
+  }
+}
+<MyComponent foo="bar" />; // ok
